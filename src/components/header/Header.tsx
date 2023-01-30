c20e7ed4f13b8ba3906c075e791336aa2e5f8bc0
jokj624 <jokj624@gmail.com>
2023-01-30 21:37:19 +0900

    Feat: Header component, scss 작성

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,28 @@
+import Style from '@/styles/header/Header.module.scss';
+
+const Header = () => {
+  return (
+    <div className={Style.Header}>
+      <div className={Style.Header__box} />
+      <div className={Style.Header__flexWrapper}>
+        <div className={Style.Header__flexWrapper__nickname}>Jobchae</div>
+        <div className={Style.Header__flexWrapper__stacks}>
+          Javascript, Typescript <br /> Node.js, Nest.js <br /> Docker, CI/CD
+        </div>
+        <div className={Style.Header__flexWrapper__intro}>
+          I'm a junior backend developer <br /> who loves the js ecosystem and
+          <br />
+          deep dive into Node.js and Typescripts.
+        </div>
+      </div>
+      <div className={Style.Header__rowLine} />
+      <div className={Style.Header__title}>
+        <div className={Style.Header__title__text}>Growing</div>
+        <div className={Style.Header__title__text}>Junior Backend</div>
+        <div className={Style.Header__title__text}>Developer Jobchae.</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
