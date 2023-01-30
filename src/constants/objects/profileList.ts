91b11e2783560796eeef011a9006d2649591b984
jokj624 <jokj624@gmail.com>
2023-01-31 00:25:02 +0900

    Feat: profile object

---
+++ src/constants/objects/profileList.ts
@@ -0,0 +1,8 @@
+export const PROFILE_LIST: {
+  [key: string]: string;
+} = {
+  Phone: '010 9953 1626',
+  Email: 'jokj624@gmail.com',
+  Github: 'https://github.com/jokj624',
+  Blog: 'https://blog.jobchae.kr',
+};
