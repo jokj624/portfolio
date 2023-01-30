de012f65de17c4d463cbb6f955a9fdb4c9128817
jokj624 <jokj624@gmail.com>
2023-01-30 21:38:38 +0900

    Fix: main 레이아웃 적용, 컴포넌트 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import AppLayout from '@/components/layout/AppLayout';
+import '@/styles/globals.scss';
+import '../../public/static/fonts/style.css';
+
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  );
 }
