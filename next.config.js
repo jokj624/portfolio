6b842e782100a0219a7a45d87a23fc7d3e1dfa59
jokj624 <jokj624@gmail.com>
2023-02-12 20:45:20 +0900

    Style: 불필요한 주석 제거

--- next.config.js
+++ next.config.js
@@ -5,15 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  },
-  // webpack: (config) => {
-  //   config.module.rules.push({
-  //     test: /\.svg$/,
-  //     use: ['@svgr/webpack'],
-  //   });
-
-  //   return config;
-  // },
+  }, g
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
