00a94d503b82c382bf79cfd512223cdb03a3d3f0
jokj624 <jokj624@gmail.com>
2023-02-12 20:46:29 +0900

    Style: 오탈자 제거

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  }, g
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
